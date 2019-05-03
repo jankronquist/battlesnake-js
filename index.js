71e4bc4df932c47a40770f8facd12e8bdde7ef96
Jan Kronquist <jan.kronquist@samsclub.com>
2019-05-03 16:08:30 -0700

    working random

--- index.js
+++ index.js
@@ -33,16 +33,69 @@ app.post('/start', (request, response) => {
   return response.json(data)
 })
 
+const EMPTY = ' ';
+
+const directions = {
+  up: { x: 0, y: -1 },
+  down: { x: 0, y: 1 },
+  right: { x: 1, y: 0 },
+  left: { x: -1, y: 0 },
+};
+
+
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+var shuffle = function (array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
+
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  console.log('POST');
+  const { height, width } = request.body.board;
+  
+  const board = Array(height).fill(EMPTY).map(() => Array(width).fill(EMPTY));
+  request.body.board.snakes.forEach((snake) => snake.body.forEach(({x,y}) => board[y][x] = 'S'));
 
-  // Response data
-  const data = {
-    move: 'up', // one of: ['up','down','left','right']
+  for (var retries = 0; retries<10; retries++) {
+    const move = ['up','down','left','right'][Math.floor(4*Math.random())];
+    const head = request.body.you.body[0];
+    console.log('head=', head, ' trying move=', move);
+    const newY = head.y + directions[move].y;
+    const newX = head.x + directions[move].x;
+    if (board[newY][newX] === EMPTY && newX >= 0 && newX < width && newY >= 0 && newY < height) {
+      console.log('move=', move);
+      return response.json({
+        move
+      })
+    }
   }
-
-  return response.json(data)
+  console.log('FAILEDqwe');
+  return response.json({
+    move: 'up'
+  })
 })
 
 app.post('/end', (request, response) => {
